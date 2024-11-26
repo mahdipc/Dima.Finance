5554140484d8dfedbdda5d3d73bf2a7fda780bee
mywaysdev1 <mywaysdev1@gmail.com>
2024-11-26 18:24:09 +0330

    add interface

---
+++ src/Core/Dima.Finance.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+
+
+namespace Dima.Finance.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Account> Accounts { get; }
+        DbSet<Customer> Customers { get; }
+        DbSet<Transaction> Transactions { get; }
+        DbSet<LoanApplication> LoanApplications { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Core/Dima.Finance.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,9 @@
+﻿namespace Dima.Finance.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+        Task<bool> IsInRoleAsync(string userId, string role);
+        Task<bool> AuthorizeAsync(string userId, string policyName);
+    }
+}
